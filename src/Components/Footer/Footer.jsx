49d92240550fb7c5e9ba595b9051b9c4450710b7
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2024-11-16 13:25:51 +0530

    footer change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,41 +1,20 @@
 import './Footer.css'
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaTwitter,
-  FaLinkedin,
-} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer">
 
-      <div className="menuItems">
-        <li className="menuItem">Terms Of Use</li>
-        <li className="menuItem">Privacy-Policy</li>
-        <li className="menuItem">About</li>
-        <li className="menuItem">FAQ</li>
+        <h1 className='tit'>LOT<span className='redd '>HUB</span></h1>
+
+      <div className="info">
+        <li>Welcome to LoHub, your ultimate destination for endless cinematic adventures. Explore a vast library of movies and TV shows, stream effortlessly, and dive into a world of entertainment curated just for you. With personalized recommendations and a sleek, user-friendly interface, LoHub makes every viewing experience unforgettable.</li>
       </div>
-      <div className="infoText">
-        <li>Welcome to Movix, your premier destination for a cinematic journey at your
-          fingertips. Dive into an extensive library, enjoy seamless streaming, and
-          explore a world of entertainment without borders. Movix offers personalized
-          recommendations in a user-friendly interface, making every click a gateway
-          to a tailored cinematic experience.</li>
-      </div>
-      <div className="socialIcons">
-        <span className="icon">
-          <FaFacebookF />
-        </span>
-        <span className="icon">
-          <FaInstagram />
-        </span>
-        <span className="icon">
-          <FaTwitter />
-        </span>
-        <span className="icon">
-          <FaLinkedin />
-        </span>
+
+      <div className="links">
+        <a href="#">Privacy Policy</a>
+        <a href="#">FAQ</a>
+        <a href="#">About</a>
+        <a href="#">Contact Us</a>
       </div>
 
     </footer>
