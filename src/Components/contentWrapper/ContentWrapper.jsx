cf2e63d6c5c2c86ca05746894a082acaa85a2062
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2024-11-18 00:25:41 +0530

    add popular top_rated

--- src/Components/contentWrapper/ContentWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./ContentWrapper.css";
-
-const ContentWrapper = ({ children }) => {
-    return <div className="contentWrapper">{children}</div>;
-};
-
-export default ContentWrapper;
