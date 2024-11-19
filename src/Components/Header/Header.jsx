2ba2dae2decaf9e6b05d2037e01341e462d8777f
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2024-11-19 11:31:28 +0530

    adding alignment

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,24 +6,34 @@ import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 import './Header.css'
 
+
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
+  const navigate = useNavigate();
+  const [showSearch, setShowSearch] = useState("");
 
   const toggleMobileMenu = () => {
     setMobileMenu(!mobileMenu);
   };
-
+  const openSearch = () => {
+    setShowSearch(true);
+};
+const searchQuery = (event) => {
+  if (event.key === "Enter" && query.length > 0) {
+    navigate(`/search/${query}`)
+  }
+}
 
   return (
     <nav>
       <h1>LOT<span className='redd '>HUB</span></h1>
       <div className="MenuItems">
-        <li className="MenuItem">MOVIES</li>
-        <li className="MenuItem">TV SHOWS</li>
-        <li className="MenuItem"><HiOutlineSearch className="icon search-menu"/></li>
+        <li className="MenuItem" onClick={()=> navigate("/explore/movie")}>MOVIES</li>
+        <li className="MenuItem" onClick={()=> navigate("/explore/tv")}>TV SHOWS</li>
+        <li className="MenuItem"><HiOutlineSearch onClick={openSearch} className="icon search-menu"/></li>
       </div>
       <div className="mobileMenuItems ">
-        <HiOutlineSearch className="icon search-menu"/>
+        <HiOutlineSearch className="icon search-menu" onClick={openSearch}/>
         {mobileMenu ? 
           <VscChromeClose className="icon close-menu" onClick={toggleMobileMenu}/>
          : 
@@ -33,11 +43,28 @@ const Header = () => {
       {mobileMenu && (
         <div className="mobileView">
           <ul className="menuItems">
-            <li className="menuItem">MOVIES</li>
-            <li className="menuItem">TV SHOWS</li>
+            <li className="menuItem" onClick={()=> navigate("/explore/movie")}>MOVIES</li>
+            <li className="menuItem" onClick={()=> navigate("/explore/tv")}>TV SHOWS</li>
           </ul>
         </div>
       )}
+                  {showSearch && (
+                <div className="searchBar">
+                    
+                        <div className="searchInput">
+                            <input
+                                type="text"
+                                placeholder="Search for a movie or tv show...."
+                                onChange={(e) => setQuery(e.target.value)}
+                                onKeyUp={searchQuery}
+                            />
+                            <VscChromeClose className="close-btn"
+                                onClick={() => setShowSearch(false)}
+                            />
+                        </div>
+                    
+                </div>
+            )}
     </nav>
   )
 }
